29b1514c1e283b6cf55ff0d257ba32eff20ca727
solvicta <solvicta@gmail.com>
2025-11-27 18:49:53 -0500

    Added some methods for custom tree stuff.

--- src/main/java/net/lunavicta/thebiomesbetween/block/ModBlocks.java
+++ src/main/java/net/lunavicta/thebiomesbetween/block/ModBlocks.java
@@ -2,6 +2,7 @@ package net.lunavicta.thebiomesbetween.block;
 
 
 import net.lunavicta.thebiomesbetween.block.custom.ModFlammableRotatedPillarBlock;
+import net.lunavicta.thebiomesbetween.item.custom.ModItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.BlockItem;
@@ -11,24 +12,27 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.SaplingBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 
+import java.util.function.Supplier;
+
 public class ModBlocks {
     /* LOGS & WOOD */
     public static final DeferredBlock<Block> BLOODWOOD_LOG = registerBlock("bloodwood_log",
-            () -> new ModFlammableRotatedPillarBlock(BlockBehavior.Properties.ofFullCopy(Blocks.OAK_LOG)));
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LOG)));
     public static final DeferredBlock<Block> BLOODWOOD_WOOD = registerBlock("bloodwood_wood",
-            () -> new ModFlammableRotatedPillarBlock(BlockBehavior.Properties.ofFullCopy(Blocks.OAK_WOOD)));
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_WOOD)));
     public static final DeferredBlock<Block> STRIPPED_BLOODWOOD_LOG = registerBlock("stripped_bloodwood_log",
-            () -> new ModFlammableRotatedPillarBlock(BlockBehavior.Properties.ofFullCopy(Blocks.STRIPPED_OAK_LOG)));
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.STRIPPED_OAK_LOG)));
     public static final DeferredBlock<Block> STRIPPED_BLOODWOOD_WOOD = registerBlock("stripped_bloodwood_wood",
-            () -> new ModFlammableRotatedPillarBlock(BlockBehavior.Properties.ofFullCopy(Blocks.STRIPPED_OAK_WOOD)));
+            () -> new ModFlammableRotatedPillarBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.STRIPPED_OAK_WOOD)));
 
     /* LEAVES AND PLANKS */
     public static final DeferredBlock<Block> BLOODWOOD_PLANKS = registerBlock("bloodwood_planks",
-            () -> new Block(BlockBehavior.Properties.ofFullCopy(Blocks.OAK_PLANKS)) {
+            () -> new Block(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_PLANKS)) {
                 @Override
                 public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
                     return true;
@@ -46,7 +50,7 @@ public class ModBlocks {
             });
 
     public static final DeferredBlock<Block> BLOODWOOD_LEAVES = registerBlock("bloodwood_leaves",
-            () -> new LeavesBlock(BlockBehavior.Properties.ofFullCopy(Blocks.OAK_LEAVES)) {
+            () -> new LeavesBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_LEAVES)) {
                 @Override
                 public boolean isFlammable(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
                     return true;
@@ -64,7 +68,7 @@ public class ModBlocks {
             });
     /* SAPLING */
     public static final DeferredBlock<Block> BLOODWOOD_SAPLING= registerBlock("bloodwood_sapling",
-            () -> new SaplingBlock(, BlockBehavior.Properties.ofFullCopy(Blocks.OAK_SAPLING)));
+            () -> new SaplingBlock(, BlockBehaviour.Properties.ofFullCopy(Blocks.OAK_SAPLING)));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
@@ -72,7 +76,7 @@ public class ModBlocks {
         return toReturn;
     }
 
-    private static <T extends Block> void registerBlockItems(String name, DeferredBlock<T> block) {
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
     }
 
